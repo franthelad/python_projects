345e8f5e044f1366c7bcaad149cc8bae10d2f92e
franthelad <120594638+franthelad@users.noreply.github.com>
2024-02-18 18:27:24 +0100

    generador de primos hasta infinito

---
+++ generador_primos_infinitos.py
@@ -0,0 +1,24 @@
+def contador_infinito(num):
+    while True:
+        yield num
+        num += 1
+
+contador = contador_infinito(2)
+
+def primos():
+    primos = []
+    
+    for numerador in contador: 
+        divisores = []
+        for divisor in range(1, numerador+1): 
+            if numerador % divisor == 0:
+                divisores.append(divisor)
+            if len(divisores) > 2:
+                break
+        if len(divisores) == 2:
+            yield numerador
+            primos.append(numerador)
+        
+
+enumerador_primos = primos()
+print(next(enumerador_primos))
